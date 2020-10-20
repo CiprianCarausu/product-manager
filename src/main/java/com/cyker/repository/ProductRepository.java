f7e715b4e648814f478933838aade53b71988691
Razvan Ciprian Augustin Carausu <Ciprian.Carausu@endava.com>
2020-10-20 14:28:34 +0300

    domain&repository added

--- src/main/java/com/cyker/repository/ProductRepository.java
+++ src/main/java/com/cyker/repository/ProductRepository.java
@@ -1,2 +1,7 @@
-package com.cyker.repository;public interface ProductRepository {
+package com.cyker.repository;
+
+import com.cyker.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
